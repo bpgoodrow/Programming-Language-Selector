db2d892ce3d7072ae848129d0921aae4cf9d596f
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-03 13:55:18 -0700

    add trigger(reset) for both forms, WIP user-input only shows suggestion1

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,24 @@ $(document).ready(function() {
     $("#output-user-info").show();
 
     event.preventDefault();
+    $("form#user-info").trigger("reset");
   });
 
   $("form#user-input").submit(function(event) {
     event.preventDefault();
+    $("form#user-input").trigger("reset");
     const learningStyle = $("select#learning-style").val();
     const optPes = $("select#opt-pes").val();
     const rainSun = $("select#rain-sun").val();
 
-    if (learningStyle === 'Logical' || rainSun === 'Rain'); {
+    if (learningStyle === "Logical" || rainSun === "Rain"); {
       $("#suggestion1").show();
     } elseif (learningstyle === 'Creative' && optPes === 'Optimist'); {
       $("#suggestion2").show();
-    }  elseif (OptPes === 'Pessimist'); {
-        $("#suggestion2").show();
+    } elseif (OptPes === 'Pessimist'); {
+      $("#suggestion3").show();
     };
+    // $("form#user-input").trigger("reset");
 
     // $("#suggestion1").show();
     // $("#suggestion2").show();
