13adb39764e99f105b069d46b1e86df298304ebc
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-03 15:10:38 -0700

    WIP form not returning suggstions, debugging from console

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
     const learningStyle = $("select#learning-style").val();
     const optPes = $("select#opt-pes").val();
     const rainSun = $("select#rain-sun").val();
-    const option = $("input:option[name=option]:selected").val();
+    // const option = $("input:option[name=option]:selected").val();
 
-    if (learningStyle === "Logical" || rainSun === "Rain"); {
+    if (learningStyle === "Logical") {
       $("#suggestion1").show();
-    } elseif (learningstyle === "Creative" && optPes === "Optimist"); {
+    } else if (learningstyle === "Creative" && optPes === "Optimist") {
       $("#suggestion2").show();
-    } elseif (OptPes === "Pessimist"); {
+    } else (OptPes === "Pessimist"); {
       $("#suggestion3").show();
     };
   });
