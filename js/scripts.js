d5e8ae1a691b579a8aba58fa84a152f087d9a9b4
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-03 13:33:07 -0700

    Add boolean logic for form#user-input, javascript WIP

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,23 @@ $(document).ready(function() {
   });
 
   $("form#user-input").submit(function(event) {
+    event.preventDefault();
     const learningStyle = $("select#learning-style").val();
     const optPes = $("select#opt-pes").val();
     const rainSun = $("select#rain-sun").val();
 
-    let reveal = "#suggestion1";
-    if (learningStyle === 'Logical' || rainSun === 'Rain');
-    else if
+    if (learningStyle === 'Logical' || rainSun === 'Rain'); {
+      $("#suggestion1").show();
+    } elseif (learningstyle === 'Creative' && optPes === 'Optimist'); {
+      $("#suggestion2").show();
+    }  elseif (OptPes === 'Pessimist'); {
+        $("#suggestion2").show();
+    };
 
-    $("#suggestion1").show();
-    $("#suggestion2").show();
-    $("#suggestion3").show();
+    // $("#suggestion1").show();
+    // $("#suggestion2").show();
+    // $("#suggestion3").show();
 
-    event.preventDefault();
+    // event.preventDefault();
   });
 });
