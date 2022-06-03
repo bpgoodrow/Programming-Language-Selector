d220492b507e000a435100be58b19bd27a8fe53a
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-03 14:37:39 -0700

    WIP add value to options for form suggestion

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,14 @@ $(document).ready(function() {
     const learningStyle = $("select#learning-style").val();
     const optPes = $("select#opt-pes").val();
     const rainSun = $("select#rain-sun").val();
+    const option = $("input:option[name=option]:selected").val();
 
     if (learningStyle === "Logical" || rainSun === "Rain"); {
       $("#suggestion1").show();
-    } elseif (learningstyle === 'Creative' && optPes === 'Optimist'); {
+    } elseif (learningstyle === "Creative" && optPes === "Optimist"); {
       $("#suggestion2").show();
-    } elseif (OptPes === 'Pessimist'); {
+    } elseif (OptPes === "Pessimist"); {
       $("#suggestion3").show();
     };
-    // $("form#user-input").trigger("reset");
-
-    // $("#suggestion1").show();
-    // $("#suggestion2").show();
-    // $("#suggestion3").show();
-
-    // event.preventDefault();
   });
 });
